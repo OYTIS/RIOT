OYTIS/RIOT-kea

boards: support for KEA128LEDLIGHTRD board
Signed-off-by: Anton Gerasimov <anton.gerasimov@here.com>

boards/kea128ledlightrd/board.c
@@ -1,0 +1,66 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ * Copyright (C) 2014 PHYTEC Messtechnik GmbH
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup     boards_kea128ledlightrd
+ * @{
+ *
+ * @file
+ * @brief       Board specific implementations for the KEA128LEDLIGHTRD
+ *
+ * @author      Johann Fischer <j.fischer@phytec.de>
+ * @author      Anton Gerasimov <anton.gerasimov@here.com>
+ *
+ * @}
+ */
+
+#include "board.h"
+#include "periph/gpio.h"
+#include "tja1042.h"
+
+extern void SystemInit(void);
+
+#ifdef MODULE_CAN_KEA_MSCAN
+
+static tja1042_trx_t tja1042 = {
+    .trx.driver = &tja1042_driver,
+    .stb_pin = GPIO_PIN(PORT_A, 24),
+};
+
+const candev_kea_conf_t candev_kea_config[] = {
+    {
+        .clock_freq = CLOCK_BUSCLOCK / 2,
+        .params.name = "can0",
+        .params.trx = (can_trx_t *)&tja1042,
+    },
+};
+
+#endif
+
+void board_init(void)
+{
+    /* initialize clocks */
+    SystemInit();
+
+    /* initialize the CPU core */
+    cpu_init();
+
+    /* disable NMI on the pin used for LED2 */
+    SIM->SOPT0 &= (~SIM_SOPT0_NMIE_MASK);
+
+    /* initialize and turn off the on-board RGB-LED */
+    gpio_init(LED0_PIN, GPIO_OUT);
+    gpio_init(LED1_PIN, GPIO_OUT);
+    gpio_init(LED2_PIN, GPIO_OUT);
+    gpio_init(LED3_PIN, GPIO_OUT);
+    gpio_set(LED0_PIN);
+    gpio_set(LED1_PIN);
+    gpio_set(LED2_PIN);
+    gpio_set(LED3_PIN);
+}

boards/kea128ledlightrd/include/periph_conf.h
@@ -1,0 +1,76 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ * Copyright (C) 2015 PHYTEC Messtechnik GmbH
+ * Copyright (C) 2018 HERE Deutschland GmbH
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup     boards_kea128ledlightrd
+ * @{
+ *
+ * @file
+ * @name        Peripheral MCU configuration for KEA128LEDLIGHTRD
+ *
+ * @author      Johann Fischer <j.fischer@phytec.de>
+ * @author      Anton Gerasimov <anton.gerasimov@here.com>
+ */
+
+#ifndef PERIPH_CONF_H
+#define PERIPH_CONF_H
+
+#include "periph_cpu.h"
+
+#ifdef MODULE_CAN_KEA_MSCAN
+#include "mscan.h"
+#endif
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define CLOCK_CORECLOCK              (40000000ul)
+#define CLOCK_BUSCLOCK               (CLOCK_CORECLOCK / 1)
+/** @} */
+
+/**
+ * @name Timer configuration
+ * @{
+ */
+#define PIT_NUMOF               (2U)
+#define LPTMR_NUMOF             (0U)
+#define PIT_CONFIG {                 \
+        {                            \
+            .prescaler_ch = 0,       \
+            .count_ch = 1,           \
+        },                           \
+        {                            \
+            .prescaler_ch = 2,       \
+            .count_ch = 3,           \
+        },                           \
+}
+#define TIMER_NUMOF             ((PIT_NUMOF))
+
+#define PIT_BASECLOCK           (CLOCK_BUSCLOCK)
+#define PIT_ISR_0               isr_pit1
+#define PIT_ISR_1               isr_pit3
+/** @} */
+
+#ifdef MODULE_CAN_KEA_MSCAN
+/**
+ * @name CAN configuration
+ * @{
+ */
+
+extern const candev_kea_conf_t candev_kea_config[];
+#endif
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PERIPH_CONF_H */
+/** @} */

OYTIS/RIOT

boards/kea128ledlightrd/board.c
@@ -1,0 +1,66 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ * Copyright (C) 2014 PHYTEC Messtechnik GmbH
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup     boards_kea128ledlightrd
+ * @{
+ *
+ * @file
+ * @brief       Board specific implementations for the KEA128LEDLIGHTRD
+ *
+ * @author      Johann Fischer <j.fischer@phytec.de>
+ * @author      Anton Gerasimov <anton.gerasimov@here.com>
+ *
+ * @}
+ */
+
+#include "board.h"
+#include "periph/gpio.h"
+#include "tja1042.h"
+
+extern void SystemInit(void);
+
+#ifdef MODULE_PERIPH_MSCAN
+
+static tja1042_trx_t tja1042 = {
+    .trx.driver = &tja1042_driver,
+    .stb_pin = GPIO_PIN(PORT_A, 24),
+};
+
+const candev_kea_conf_t candev_kea_config[] = {
+    {
+        .clock_freq = CLOCK_BUSCLOCK / 2,
+        .params.name = "can0",
+        .params.trx = (can_trx_t *)&tja1042,
+    },
+};
+
+#endif
+
+void board_init(void)
+{
+    /* initialize clocks */
+    SystemInit();
+
+    /* initialize the CPU core */
+    cpu_init();
+
+    /* disable NMI on the pin used for LED2 */
+    SIM->SOPT0 &= (~SIM_SOPT0_NMIE_MASK);
+
+    /* initialize and turn off the on-board RGB-LED */
+    gpio_init(LED0_PIN, GPIO_OUT);
+    gpio_init(LED1_PIN, GPIO_OUT);
+    gpio_init(LED2_PIN, GPIO_OUT);
+    gpio_init(LED3_PIN, GPIO_OUT);
+    gpio_set(LED0_PIN);
+    gpio_set(LED1_PIN);
+    gpio_set(LED2_PIN);
+    gpio_set(LED3_PIN);
+}

boards/kea128ledlightrd/include/periph_conf.h
@@ -1,0 +1,76 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ * Copyright (C) 2015 PHYTEC Messtechnik GmbH
+ * Copyright (C) 2018 HERE Deutschland GmbH
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup     boards_kea128ledlightrd
+ * @{
+ *
+ * @file
+ * @name        Peripheral MCU configuration for KEA128LEDLIGHTRD
+ *
+ * @author      Johann Fischer <j.fischer@phytec.de>
+ * @author      Anton Gerasimov <anton.gerasimov@here.com>
+ */
+
+#ifndef PERIPH_CONF_H
+#define PERIPH_CONF_H
+
+#include "periph_cpu.h"
+
+#ifdef MODULE_PERIPH_MSCAN
+#include "mscan.h"
+#endif
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define CLOCK_CORECLOCK              (40000000ul)
+#define CLOCK_BUSCLOCK               (CLOCK_CORECLOCK / 1)
+/** @} */
+
+/**
+ * @name Timer configuration
+ * @{
+ */
+#define PIT_NUMOF               (2U)
+#define LPTMR_NUMOF             (0U)
+#define PIT_CONFIG {                 \
+        {                            \
+            .prescaler_ch = 0,       \
+            .count_ch = 1,           \
+        },                           \
+        {                            \
+            .prescaler_ch = 2,       \
+            .count_ch = 3,           \
+        },                           \
+}
+#define TIMER_NUMOF             ((PIT_NUMOF))
+
+#define PIT_BASECLOCK           (CLOCK_BUSCLOCK)
+#define PIT_ISR_0               isr_pit1
+#define PIT_ISR_1               isr_pit3
+/** @} */
+
+#ifdef MODULE_PERIPH_MSCAN
+/**
+ * @name CAN configuration
+ * @{
+ */
+
+extern const candev_kea_conf_t candev_kea_config[];
+#endif
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PERIPH_CONF_H */
+/** @} */